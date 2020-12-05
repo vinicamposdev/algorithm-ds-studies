ae1bc63f6cded462e491c8e5d44732a646a89c4e
vmo-campos <viniciusmoliveirac@hotmail.com>
2020-12-05 11:11:57 -0300

    Removing recursivity and add iterativily way

--- src/challenges/sock-merchant/js/main.js
+++ src/challenges/sock-merchant/js/main.js
@@ -1,19 +1,20 @@
-function sockMerchant(n, ar, matchingPairs = 0) {
-  if (n === 0) {
-    return matchingPairs;
-  }
-  const isMatchingPair = ar[n - 1] === ar[n - 2];
-  if (isMatchingPair) {
-    ar.pop();
-  }
-  ar.pop();
+const merchantUniques = {};
+let totalMerchantPairs = 0;
 
-  return sockMerchant(ar.length, ar, matchingPairs + isMatchingPair);
+function sockMerchant(n, ar) {
+  for (var i = 0; i < n; i++) {
+    if (merchantUniques.hasOwnProperty(ar[i])) {
+      totalMerchantPairs++;
+      delete merchantUniques[ar[i]];
+    } else {
+      merchantUniques[ar[i]] = 0;
+    }
+  }
+  return totalMerchantPairs;
 }
 
 (main = () => {
   const n = 9;
   const ar = [10, 20, 20, 10, 10, 30, 50, 10, 20];
-  const result = sockMerchant(n, ar.sort());
-  console.log(result);
+  const result = sockMerchant(n, ar);
 })();
